8f8fc475110c760bc80dff57e0b04d65b931b3bb
takeshi <jagabataa0141@gmail.com>
2020-03-21 16:27:07 +0900

    https://qiita.com/Futo_Horio/items/bff1ce1d2e1b219b243d コピペカメラ

--- pwa/main.js
+++ pwa/main.js
@@ -1,2 +1,29 @@
-alert("hello world!");
-console.log("hello world!")
+navigator.mediaDevices = navigator.mediaDevices || ((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
+   getUserMedia: function(c) {
+     return new Promise(function(y, n) {
+       (navigator.mozGetUserMedia ||
+        navigator.webkitGetUserMedia).call(navigator, c, y, n);
+     });
+   }
+} : null);
+
+if (!navigator.mediaDevices) {
+  console.log("getUserMedia() not supported.");
+  return;
+}
+
+// Prefer camera resolution nearest to 1280x720.
+
+var constraints = { audio: true, video: { width: 1280, height: 720 } };
+
+navigator.mediaDevices.getUserMedia(constraints)
+.then(function(stream) {
+  var video = document.querySelector('video');
+  video.srcObject = stream
+  video.onloadedmetadata = function(e) {
+    video.play();
+  };
+})
+.catch(function(err) {
+  console.log(err.name + ": " + err.message);
+});
