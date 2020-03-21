fff09f6b3a95b5b5c40fd090ca61a8dbac75022b
takeshi <jagabataa0141@gmail.com>
2020-03-21 15:48:25 +0900

    Uncaught (in promise) TypeError: Request failed と検索して一番上の対策をした

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@ self.addEventListener('install', function(event) {
     caches.open(CACHE_NAME)
       .then(function(cache) {
         console.log('Opened cache');
-        return cache.addAll(urlsToCache);
+        return cache.addAll(urlsToCache.map(url => new Request(url, {credentials: 'same-origin'})));
       })
   );
 });
