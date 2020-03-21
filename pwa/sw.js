cd1ce6e23daf8be20c445c64297ab23409669646
takeshi <jagabataa0141@gmail.com>
2020-03-21 15:20:23 +0900

    hello world

---
+++ pwa/sw.js
@@ -0,0 +1,33 @@
+//https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/pwa/',
+  '/pwa/css/style.css',
+  '/pwa/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
