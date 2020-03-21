00528f74a5079db2a5d6a2d800584b2bdde0ce86
takeshi <jagabataa0141@gmail.com>
2020-03-21 15:59:08 +0900

    https://github.com/kazaoki/pwa/blob/master/sw.js をコピペ

--- sw.js
+++ sw.js
@@ -1,30 +1,27 @@
-var CACHE_NAME = 'my-site-cache-v1';
+var CACHE_NAME = 'pwa-sample-caches';
 var urlsToCache = [
-  '/',
-  '/main.js'
+	'/githubPages/',
+	'/githubPages/drawer.js'
 ];
 
+// インストール処理
 self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache.map(url => new Request(url, {credentials: 'same-origin'})));
-      })
-  );
+	event.waitUntil(
+		caches
+			.open(CACHE_NAME)
+			.then(function(cache) {
+				return cache.addAll(urlsToCache);
+			})
+	);
 });
 
+// リソースフェッチ時のキャッシュロード処理
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then(function(response) {
+				return response ? response : fetch(event.request);
+			})
+	);
 });
