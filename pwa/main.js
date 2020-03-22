b209c653df36d7de434ede17657e88b05f372bfe
takeshi <jagabataa0141@gmail.com>
2020-03-22 17:58:43 +0900

    よおわからｎ

--- pwa/main.js
+++ pwa/main.js
@@ -1,29 +1,3 @@
-/*
-function openCamera() {
-    var video = document.querySelector('video');
-    navigator.mediaDevices = navigator.mediaDevices
-    || ((navigator.mozGetUserMedia 
-    || navigator.webkitGetUserMedia) ? {
-        getUserMedia: function(c) {
-            return new Promise(function(y, n) {
-                (navigator.mozGetUserMedia ||
-                navigator.webkitGetUserMedia).call(navigator, c, y, n);
-            });
-        }
-    } : null);
-    var constraints = { video: { facingMode: 'user', aspectRatio:{exact: window.innerHeight / window.innerWidth} } };
-    navigator.mediaDevices.getUserMedia(constraints)
-        .then(function(stream) {
-            video.srcObject = stream;
-            video.onloadedmetadata = function(e) {
-                video.play();
-            };
-        })
-        .catch(function(err) {
-            console.log(err);
-        });
-}
-*/
 function opencvCamera() {
     var video = document.querySelector('video');
         navigator.mediaDevices = navigator.mediaDevices
