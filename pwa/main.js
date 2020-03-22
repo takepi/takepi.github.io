1159dc3abfd89356294ce806b4a051eb5566fe3e
takeshi <jagabataa0141@gmail.com>
2020-03-23 00:24:41 +0900

    opencv/index.htmlではopencv.js使ってgrayスケールできたけどサイズが意味不明

--- pwa/main.js
+++ pwa/main.js
@@ -1,15 +1,5 @@
 function opencvCamera() {
-    var video = document.querySelector('video');
-        navigator.mediaDevices = navigator.mediaDevices
-    || ((navigator.mozGetUserMedia 
-    || navigator.webkitGetUserMedia) ? {
-        getUserMedia: function(c) {
-            return new Promise(function(y, n) {
-                (navigator.mozGetUserMedia ||
-                navigator.webkitGetUserMedia).call(navigator, c, y, n);
-            });
-        }
-    } : null);
+    var video = document.getElementById('video');
     var constraints = { video: { facingMode: 'user'} };
     navigator.mediaDevices.getUserMedia(constraints)
         .then(function(stream) {
@@ -22,9 +12,10 @@ function opencvCamera() {
             console.log(err);
         });
     
+    let canvasFrame = document.getElementById("canvasOutput");
+    let context = canvasFrame.getContext("2d");
     let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-    let dst = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-    let gray = new cv.Mat(); 
+    let dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
     let cap = new cv.VideoCapture(video)
     const FPS = 30;
     
@@ -37,8 +28,8 @@ function opencvCamera() {
         }
     let begin = Date.now();
     cap.read(src);
-    src.copyTo(dst);
-    cv.cvtColor(dst, gray, cv.COLOR_RGBA2GRAY, 0);
+//    src.copyTo(dst);
+    cv.cvtColor(dst, dst, cv.COLOR_RGBA2GRAY);
     cv.imshow("canvasOutput", dst);
     let delay = 1000/FPS - (Date.now() - begin);
     } catch (err) {
